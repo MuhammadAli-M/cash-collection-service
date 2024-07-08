2dab3e3c2efa46ce2fb39df8bd86d83aa167669f
Muhammad Adam <muhammadalyadam@gmail.com>
2024-07-08 13:32:32 +0300

    feat: Add tasks API to get collected tasks for a collector

--- src/domains/collection/infra/controllers/tasks_retrieval.py
+++ src/domains/collection/infra/controllers/tasks_retrieval.py
@@ -1,20 +1,36 @@
 import logging
 from http import HTTPStatus
 
+from rest_framework import serializers
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from domains.collection.infra.usecases.tasks_retrieval import \
+    TasksRetrievalRequest, TasksRetrieval
+
+
+class TasksRetrievalSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField(required=True)
+
+    def create(self, validated_data):
+        return TasksRetrievalRequest(**validated_data)
+
 
 class TasksRetrievalAPIView(GenericAPIView):
     permission_classes = [IsAuthenticated]
+    serializer_class = TasksRetrievalSerializer
 
     def get(self, http_request, *args, **kwargs):
-        # usecase = CollectionUseCase()
+        usecase = TasksRetrieval()
         try:
-            # response = usecase.execute().dict()
-            print("requets called")
-            return Response(status=HTTPStatus.OK)
+            data = dict(user_id=http_request.user.id)
+            serializer = self.get_serializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            usecase_request = serializer.save()
+            response = usecase.execute(usecase_request).dict()
+            ## TODO fix to include data in response
+            return Response({"data": response}, status=HTTPStatus.OK)
         except Exception as exc:
             logging.error(f"uncaught exception: {exc}")
             raise exc
