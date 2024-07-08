7a4fbc6b623cbe7365b2da6433488c13144f870d
Muhammad Adam <muhammadalyadam@gmail.com>
2024-07-08 11:17:08 +0300

    build: Introduce pydantic for some benefits with inheritance

--- src/domains/collection/entities/task.py
+++ src/domains/collection/entities/task.py
@@ -2,16 +2,17 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import Optional
 
+from pydantic import BaseModel
+
 CollectorID = int
 
 
-class BaseEntity:
+class BaseEntity(BaseModel):
     id: Optional[int] = None
     created_at: Optional[datetime] = None
     updated_at: Optional[datetime] = None
 
 
-@dataclass
 class Task(BaseEntity):
     collector_id: CollectorID
     is_collected: bool = False
