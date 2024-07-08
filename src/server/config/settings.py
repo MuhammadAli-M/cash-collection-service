96d104aa9283b147fae3e39859ca1f66995c65d4
Muhammad Adam <muhammadalyadam@gmail.com>
2024-07-08 07:44:40 +0300

    feat: Add simplejwt to handle jwt authentication

--- src/server/config/settings.py
+++ src/server/config/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
 ]
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
