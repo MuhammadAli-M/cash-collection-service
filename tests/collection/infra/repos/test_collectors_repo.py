8e3b9791d59e85afe673e8a4e40f6b0414f087ae
Muhammad Adam <muhammadalyadam@gmail.com>
2024-07-16 11:26:00 +0300

    test: Fix collectors repo tests to depend on the entity model

--- tests/collection/infra/repos/test_collectors_repo.py
+++ tests/collection/infra/repos/test_collectors_repo.py
@@ -5,8 +5,7 @@ from domains.collection.infra.repos.collector_dao import CollectorDao
 from domains.collection.infra.repos.collectors_repo import CollectorsRepo
 from domains.collection.infra.repos.status_dao import StatusDao
 from tests.collection.common.models_cleaner import clear_tasks, clear_users
-from tests.collection.infra.repos.fixtures import create_collector, \
-    create_status
+from tests.collection.entities.fixtures import create_collector, create_status
 
 
 class CollectorsRepoTest(TestCase):
@@ -17,21 +16,21 @@ class CollectorsRepoTest(TestCase):
 
     def test_get_collector_works_if_existing(self):
         # arrange
-        collectorDbo = create_collector()
+        collector = create_collector()
         dao_mock = Mock(spec=CollectorDao)
-        dao_mock.get_collector = MagicMock(return_value=collectorDbo)
+        dao_mock.get_collector = MagicMock(return_value=collector)
         repo = CollectorsRepo(dao=dao_mock)
 
         # act
-        retrieved = repo.get_collector(collector_id=collectorDbo.id)
+        retrieved = repo.get_collector(collector_id=collector.id)
 
         # assert
-        self.assertEqual(retrieved.id, collectorDbo.id)
-        self.assertEqual(retrieved.amount, collectorDbo.amount)
-        self.assertEqual(retrieved.user_id, collectorDbo.user_id)
-        self.assertEqual(retrieved.created_at, collectorDbo.created_at)
-        self.assertEqual(retrieved.updated_at, collectorDbo.updated_at)
-        dao_mock.get_collector.assert_called_once_with(collector_id=collectorDbo.id)
+        self.assertEqual(retrieved.id, collector.id)
+        self.assertEqual(retrieved.amount, collector.amount)
+        self.assertEqual(retrieved.user_id, collector.user_id)
+        self.assertEqual(retrieved.created_at, collector.created_at)
+        self.assertEqual(retrieved.updated_at, collector.updated_at)
+        dao_mock.get_collector.assert_called_once_with(collector_id=collector.id)
 
     def test_get_collector_works_if_not_existing(self):
         # arrange
@@ -50,26 +49,25 @@ class CollectorsRepoTest(TestCase):
 
     def test_get_collector_by_user_id_works_if_existing(self):
         # arrange
-        collectorDbo = create_collector()
+        collector = create_collector()
         dao_mock = Mock(spec=CollectorDao)
-        dao_mock.get_collector_by_user_id = MagicMock(return_value=collectorDbo)
+        dao_mock.get_collector_by_user_id = MagicMock(return_value=collector)
         repo = CollectorsRepo(dao=dao_mock)
 
         # act
-        retrieved = repo.get_collector_by_user_id(user_id=collectorDbo.user_id)
+        retrieved = repo.get_collector_by_user_id(user_id=collector.user_id)
 
         # assert
-        self.assertEqual(retrieved.id, collectorDbo.id)
-        self.assertEqual(retrieved.amount, collectorDbo.amount)
-        self.assertEqual(retrieved.user_id, collectorDbo.user_id)
-        self.assertEqual(retrieved.created_at, collectorDbo.created_at)
-        self.assertEqual(retrieved.updated_at, collectorDbo.updated_at)
+        self.assertEqual(retrieved.id, collector.id)
+        self.assertEqual(retrieved.amount, collector.amount)
+        self.assertEqual(retrieved.user_id, collector.user_id)
+        self.assertEqual(retrieved.created_at, collector.created_at)
+        self.assertEqual(retrieved.updated_at, collector.updated_at)
         dao_mock.get_collector_by_user_id.assert_called_once_with(
-            user_id=collectorDbo.user_id)
+            user_id=collector.user_id)
 
     def test_get_collector_by_user_id_works_if_not_existing(self):
         # arrange
-        collectorDbo = create_collector()
         dao_mock = Mock(spec=CollectorDao)
         dao_mock.get_collector_by_user_id = MagicMock(return_value=None)
         repo = CollectorsRepo(dao=dao_mock)
@@ -87,24 +85,24 @@ class CollectorsRepoTest(TestCase):
     def test_get_latest_status_works(self):
         # arrange
         collector = create_collector()
-        status1 = create_status(is_frozen=True, is_active=False, collector=collector)
-        status2 = create_status(is_frozen=True, is_active=False, collector=collector)
+        status1 = create_status(is_frozen=True, is_active=False, collector_id=collector.id)
+        status2 = create_status(is_frozen=True, is_active=False,  collector_id=collector.id)
 
         status_dao_mock = Mock(spec=StatusDao)
         status_dao_mock.get_latest = MagicMock(return_value=status2)
         repo = CollectorsRepo(status_dao=status_dao_mock)
 
         # act
-        status = repo.get_latest_status(collector_id=collector.id)
+        retrieved_status = repo.get_latest_status(collector_id=collector.id)
 
         # assert
         expected_status = status2
-        self.assertEqual(status.collector_id, collector.id)
-        self.assertEqual(status.due_at, expected_status.due_at)
-        self.assertEqual(status.is_frozen, expected_status.is_frozen)
-        self.assertEqual(status.is_active, expected_status.is_active)
-        self.assertEqual(status.created_at, expected_status.created_at)
-        self.assertEqual(status.updated_at, expected_status.updated_at)
+        self.assertEqual(retrieved_status.collector_id, collector.id)
+        self.assertEqual(retrieved_status.due_at, expected_status.due_at)
+        self.assertEqual(retrieved_status.is_frozen, expected_status.is_frozen)
+        self.assertEqual(retrieved_status.is_active, expected_status.is_active)
+        self.assertEqual(retrieved_status.created_at, expected_status.created_at)
+        self.assertEqual(retrieved_status.updated_at, expected_status.updated_at)
         status_dao_mock.get_latest.assert_called_once_with(
             collector_id=collector.id
         )
